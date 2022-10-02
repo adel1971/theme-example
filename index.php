4c63e2044f45ff6d6c653d845688ac8a0bc9634b
melakadil <adil-salih@hotmail.com>
2022-10-02 15:43:23 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<article>
+    <?php
+        if( have_posts() ):
+            while ( have_posts() ):
+                the_post();
+        ?>
+    <h1><?php the_title(); ?></h1>
+
+    <article><?php the_content(); ?></article>
+    <?php
+            endwhile;
+            endif;
+    ?>
+</article>
+</body>
+</html>
