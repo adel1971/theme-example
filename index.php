ac6b5a6a8758be633ff86debcd05dfe615048d71
melakadil <adil-salih@hotmail.com>
2022-10-04 01:17:53 +0300

    first commit

--- index.php
+++ index.php
@@ -1,25 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-    <?php wp_head(); ?>
-</head>
-<body>
+<?php
+get_header();
+?>
 
-
+<main>
     <?php
         if( have_posts() ):
             while ( have_posts() ):
                 the_post();
         ?>
     <h1><?php the_title(); ?></h1>
-
     <article><?php the_content(); ?></article>
     <?php
             endwhile;
             endif;
     ?>
-
-</body>
-</html>
+</main>
+<?php
+get_sidebar();
+get_footer();
+?>
---
+++ sidebar.php
@@ -0,0 +1,3 @@
+<aside>
+    <h2>Ei niin tarkeeta</h2>
+</aside>
